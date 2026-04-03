3c9c9221e8576bb41528356648ea46b33d385d11
keefalegends <keefayoura3@gmail.com>
2026-04-03 14:50:40 +0700

    adding all build-up source code.

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host:     process.env.RDS_HOSTNAME || "inventory.c70ukmgqyg53.us-east-1.rds.amazonaws.com",
+  host:     process.env.RDS_HOSTNAME,
   port:     process.env.RDS_PORT     || 5432,
-  database: process.env.RDS_DB_NAME  || "inventory",
-  user:     process.env.RDS_USERNAME || "postgres",
-  password: process.env.RDS_PASSWORD || "postgrespass",
+  database: process.env.RDS_DB_NAME,
+  user:     process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
   ssl: process.env.NODE_ENV === 'production'
     ? { rejectUnauthorized: false }
     : false,
